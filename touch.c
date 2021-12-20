1981fe4fde270fdad304eb4fa119028d53d01d92
Younes So <younes.creed.box@gmail.com>
2021-12-21 01:13:54 +0330

    new files added

--- touch.c
+++ touch.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+void printlist(char contacts [100][3][100]){
+    
+    
+}
 
 
 int main(){
+    char contacts[100][3][100];
+    char order[10];
+    int i = 0;
+    scanf("%s" , &order);
+    if(strcmp(order , "add") == 0){
+        scanf("%s %s %s" , &contacts[i][0] , &contacts[i][1] , &contacts[i][2] );
+    }
 
-    printf("salam dada");
 
 
 }
