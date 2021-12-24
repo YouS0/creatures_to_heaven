22e168ad62777b225d5883b40665df181aee6115
ALNSR02 <96082688+ALNSR02@users.noreply.github.com>
2021-12-24 15:30:27 +0330

    Update getPostion Function.c

--- getPostion Function.c
+++ getPostion Function.c
@@ -23,7 +23,7 @@ void getPosition(int c[2], char type) {
     int steps = -1;
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 20; j++) {
-            if (board[i][j] == 'H' && (steps == -1 || distance[i][j] < steps)) {
+            if (board[i][j] == type && (steps == -1 || distance[i][j] < steps)) {
                 unc[0] = i;
                 unc[1] = j;
                 steps = distance[i][j];
